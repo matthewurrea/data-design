74190316ae07da889482187b47b270789788f14c
Matthew Urrea <matt.urrea.code@gmail.com>
2020-01-20 06:57:03 -0700

    created DDL scripts, revised ERD img

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -10,7 +10,33 @@
 	<a href="index.php">Home</a>
 </div>
 <h3>Entity Relationship Diagram</h3>
+<div>
+	<h4>PROFILE: </h4>
+	<ol>
+		<li>ProfileID</li>
+		<li>ProfileActivationToken</li>
+		<li>ProfileEmail</li>
+		<li>ProfileHash</li>
+	</ol>
+</div>
+<div>
+	<h4>ALBUM: </h4>
+	<ol>
+		<li>AlbumID</li>
+		<li>AlbumProfileID</li>
+		<li>AlbumDateReleased</li>
+	</ol>
+</div>
+<div>
+	<h4>WISHLIST</h4>
+	<ol>
+		<li>WishlistAlbumID</li>
+		<li>WishlistProfileCollectionID</li>
+		<li>WishlistDateAdded</li>
+	</ol>
+</div>
 <img src="image/bandcampERD.png" alt="Entity Relationship Diagram">
+
 
 </body>
 </html>
